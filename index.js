7e12fa2c81f992deda77cc40ff81235926643c8d
Daniel <dvalenca@gmail.com>
2023-07-17 16:58:40 -0300

    pequenas correções necessárias

--- index.js
+++ index.js
@@ -6,39 +6,13 @@ const app = express();
 const cyclistCountsRouter = require('./cyclist-counts');
 const cyclistCountsEditionsRouter = require('./cyclist-counts-editions');
 
-const port = 3001; // Defina a porta desejada para a API
+const port = 3000; // Defina a porta desejada para a API
 
 // Registre a rota "cyclist-count"
 app.use('/cyclist-counts', cyclistCountsRouter);
 
 // Registre a rota "cyclist-count"
 app.use('/cyclist-counts-editions', cyclistCountsEditionsRouter);
-
-// Rota para listar todos os endpoints
-app.get('/', (req, res) => {
-  const endpoints = [];
-
-  // Percorra a lista de rotas registradas
-  app._router.stack.forEach((route) => {
-      console.log(route)
-    if (route.route && route.route.path) {
-      // Obtenha as informações sobre cada rota
-      const method = Object.keys(route.route.methods)[0].toUpperCase();
-      const path = route.route.path;
-
-      // Crie um objeto com as informações da rota
-      const endpoint = {
-        method,
-        path,
-      };
-      // Adicione o objeto à lista de endpoints
-      endpoints.push(endpoint);
-    }
-  });
-
-  // Retorne a lista de endpoints como resposta
-  res.json(endpoints);
-});
 
 app.listen(port, () => {
   console.log(`API rodando na porta ${port}`);
