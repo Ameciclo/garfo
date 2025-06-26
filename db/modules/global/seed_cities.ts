41886e4f0f5d48fdc7ba59ab6f51daf8a5d94310
Daniel Valença <dvalenca@gmail.com>
2025-06-26 15:34:43 -0300

    remoção do seed de geo das cidades

--- db/modules/global/seed_cities.ts
+++ db/modules/global/seed_cities.ts
@@ -1,10 +1,7 @@
 // db/seed/global/seed.ts
 import * as schema from "./table_cities";
 import { db, readCsv } from "../../utils";
-import fs from "fs/promises";
 import path from "path";
-import { sql } from "drizzle-orm";
-import type { FeatureCollection } from "geojson";
 
 type CityCsv = {
   id: string;
@@ -29,26 +26,6 @@ export async function seedCities() {
 
   await db.insert(schema.cities).values(cities).onConflictDoNothing().execute();
   console.log(`✅ Seeded ${cities.length} cities`);
-
-  // 2) Load and apply geometries from GeoJSON
-  const geoJsonPath = path.resolve(__dirname, "geojs-100-mun.json");
-  const content = await fs.readFile(geoJsonPath, "utf-8");
-  const geo = JSON.parse(content) as FeatureCollection;
-
-  for (const feat of geo.features) {
-    const id = Number(feat.properties!.id);
-    const geometryJson = JSON.stringify(feat.geometry);
-
-    await db.execute(
-      sql`
-        UPDATE ${schema.cities}
-        SET geom = ST_SetSRID(ST_GeomFromGeoJSON(${geometryJson}), 4326)
-        WHERE id = ${id}
-      `
-    );
-  }
-
-  console.log(`✅ Updated geometries for ${geo.features.length} cities`);
 }
 
 // If run directly: `tsx db/seed/global/seed.ts`
